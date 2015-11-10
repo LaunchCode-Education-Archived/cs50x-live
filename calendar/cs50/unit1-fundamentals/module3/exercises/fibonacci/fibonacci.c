69f5c897a824913b308c5b81113eac315d25f329
Jesse Levine <jesseilev@gmail.com>
2015-11-10 17:17:53 -0600

    Update fibonacci.c

--- calendar/cs50/unit1-fundamentals/module3/exercises/fibonacci/fibonacci.c
+++ calendar/cs50/unit1-fundamentals/module3/exercises/fibonacci/fibonacci.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int fib(int n);
+
+int main(int argc, char* argv[])
+{
+  // validate
+  if (argc != 2) 
+  {
+    printf("usage: ./fibonacci n\n");
+    return 1; // return a non-zero number to indicate an error
+  }
+  
+  // get the command-line arg
+  char* argument = argv[1];
+  int n = atoi(argument);
+  
+  // compute answer and then print it
+  int answer = fib(n);
+  printf("%i\n," answer);
+
+  // all's well
+  return 0;
+}
+
+/**
+ * returns the nth term in the Fibonacci Sequence
+ */
+int fib(int n) 
+{
+  // TODO
+  return 0;
+}
