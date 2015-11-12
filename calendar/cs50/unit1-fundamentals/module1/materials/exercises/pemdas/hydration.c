4a3360ad02de4f8cbcbc352efc5a20f427d350b2
Jesse Levine <jesseilev@gmail.com>
2015-11-12 16:02:47 -0600

    Update hydration.c

--- calendar/cs50/unit1-fundamentals/module1/materials/exercises/pemdas/hydration.c
+++ calendar/cs50/unit1-fundamentals/module1/materials/exercises/pemdas/hydration.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+int main(void)
+{
+  printf("How many miles have you run so far? ");
+  float miles_run = GetFloat();
+  
+  printf("How hours ago did you start? ");
+  float hours_so_far = GetFloat();
+  
+  float mph = miles_run / hours_so_far
+  
+  hours_remaining == 26.2 - miles_run / mph;
+  
+  prtintf("At this rate, you have %f hours left.", hours_remaining);
+  
+}
