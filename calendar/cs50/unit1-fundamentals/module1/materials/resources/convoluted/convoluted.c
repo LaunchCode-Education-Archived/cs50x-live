6aa22d89a6bdb5d94370227975cc9b320aa8335f
Jesse Levine <jesseilev@gmail.com>
2015-11-12 16:45:16 -0600

    fix bugs, improve comment formatting

--- calendar/cs50/unit1-fundamentals/module1/materials/resources/convoluted/convoluted.c
+++ calendar/cs50/unit1-fundamentals/module1/materials/resources/convoluted/convoluted.c
@@ -1,46 +1,55 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int main void()
+int main(void)
 {
   
+  // Example 1 ---------------------------------------------------------------
   // this works, but is a very bad idea
   printf("What is the year of your birth? ");
-  string birthyear = GetString();
-  // don't even bother trying to understand this:
-  printf("In the year %i, you were halfway towards becoming who you are today!\n", (2016 - atoi(birthyear)) + (2016 - atoi(birthyear)) / 2);
+  string birthyear_str = GetString();
+  // this code is impossible to read because there is too much stuff crammed onto one line:
+  printf("In the year %i, you were halfway towards becoming who you are today!\n", atoi(birthyear_str) + (2016 - atoi(birthyear_str)) / 2);
+  // -------------------------------------------------------------------------
 
+
+  // Example 2.  ---------------------------------------------------------------
   // this is ever so slightly better
   printf("What is the year of your birth? ");
-  string birthyear = GetString();
-  int answer = (2016 - atoi(birthyear)) + (2016 - atoi(birthyear)) / 2
+  birthyear_str = GetString();
+  int answer = atoi(birthyear_str) + (2016 - atoi(birthyear_str)) / 2;
   printf("In the year %i, you were halfway towards becoming who you are today!\n", answer);
   
   // but still needs to be expanded more, because:
-  // -- it's impossible for someone else to understand what that math is doing.
+  // -- it's very difficult for someone else to understand what that math is doing.
   // -- if we get a bug, it will be very difficult to figure out which part of our formula is broken
-  // -- we repeat this expression twice: (2016 - atoi(birthyear))
+  // -- we repeat the same expression twice: (2016 - atoi(birthyear))
+  
+  // -------------------------------------------------------------------------
 
-  // ------------------------------
+  
+
+
+  // Example 3 ---------------------------------------------------------------
   // The following is much clearer. 
   // We break up the convulted math into multiple lines with descriptive variable names and comments
-  // ------------------------------
-  
+
   // prompt user for their birthday
   printf("What is the year of your birth? ");
   
   // get user's answer
-  string birthyear_str = GetString();
+  birthyear_str = GetString();
   
   // convert string to int
   int birthyear = atoi(birthyear_str);
   
   // calculate year at which the user was half of their current age
-  int currentYear = 2016;
-  int age = currentYear - birthyear;
+  int currentyear = 2016;
+  int age = currentyear - birthyear;
   int halfage = age / 2;
-  int halfwayYear = birthYear + halfAge;
+  int halfwayYear = birthyear + halfage;
   
   printf("In the year %i, you were halfway towards becoming who you are today!\n", halfwayYear);
   
+  // -------------------------------------------------------------------------
 }
