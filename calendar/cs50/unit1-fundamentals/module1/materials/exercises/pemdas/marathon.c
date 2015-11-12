2ea7f850f99121769f54fda321f47604fb7830f4
Jesse Levine <jesseilev@gmail.com>
2015-11-12 16:09:04 -0600

    Update marathon.c

--- calendar/cs50/unit1-fundamentals/module1/materials/exercises/pemdas/marathon.c
+++ calendar/cs50/unit1-fundamentals/module1/materials/exercises/pemdas/marathon.c
@@ -1,3 +1,9 @@
+/**
+ * marathon
+ * predicts the time at which a marathon runner will cross the finish line, 
+ * assuming they continue to run at a constant speed
+ */
+
 #include <stdio.h>
 
 int main(void)
@@ -8,8 +14,7 @@ int main(void)
   printf("How hours ago did you start? ");
   float hours_so_far = GetFloat();
   
-  float mph = miles_run / hours_so_far
-  
+  float mph = miles_run / hours_so_far;
   hours_remaining == 26.2 - miles_run / mph;
   
   prtintf("At this rate, you have %f hours left.", hours_remaining);
