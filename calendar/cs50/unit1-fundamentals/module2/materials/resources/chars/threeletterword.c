f997856e53a0aba6b0b56979e737443c1c289d56
Jesse Levine <jesseilev@gmail.com>
2015-12-03 17:19:48 -0600

    put inside main

--- calendar/cs50/unit1-fundamentals/module2/materials/resources/chars/threeletterword.c
+++ calendar/cs50/unit1-fundamentals/module2/materials/resources/chars/threeletterword.c
@@ -1,5 +1,8 @@
+#include <stdio.h>
 #include <cs50.h>
 
+int main(void)
+{
 // Our method: read in three chars, one after the other, then print them all out as part of a message.
 
 // Ask for the first char, store it
@@ -15,4 +18,6 @@ printf("char please: ");
 char thirdChar = GetChar();
 
 // print them all out
-printf(“That spells %c%c%c!”, firstChar, secondChar, thirdChar);
+printf(“That spells %c%c%c!\n”, firstChar, secondChar, thirdChar);
+
+}
