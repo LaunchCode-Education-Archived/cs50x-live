460cae902ae612487e91f3fbc5d8cfc1c29c9484
Ricky Ng <dummey@gmail.com>
2015-12-03 19:38:52 -0600

    Making use of loops to better enforce the idea using arrays.

--- calendar/cs50/unit1-fundamentals/module2/materials/exercises/array-read/array-read-after.c
+++ calendar/cs50/unit1-fundamentals/module2/materials/exercises/array-read/array-read-after.c
@@ -5,12 +5,14 @@
 
 int main (void)
 {
-  // The reason we want to do this is to group together common things. 
+  // The reason we want to do this is that it is a good idea to group similair 
+  // items together in a stronger manner than just variable names. It makes it 
+  // so that we can use loops to get the computer to do the work for us!
   int lottery_numbers[] = [1, 13, 4];
 
-  printf("The wining lottery numbers for this month: %d, %d, %d", 
-    lottery_numbers[0], 
-    lottery_numbers[1], 
-    lottery_numbers[2]
-  );
+  printf("The wining lottery numbers for this month: ");
+  for (int i = 0; i < strlen(lottery_numbers); i++) {
+    printf("%d ", lottery_numbers[i]);
+  }
+  printf("\n");
 }
