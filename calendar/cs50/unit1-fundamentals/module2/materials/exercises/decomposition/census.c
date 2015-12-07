b36140ed8f106692b1bb171748f03be719b2b01b
Jesse Levine <jesseilev@gmail.com>
2015-12-07 17:11:33 -0600

    Update census.c

--- calendar/cs50/unit1-fundamentals/module2/materials/exercises/decomposition/census.c
+++ calendar/cs50/unit1-fundamentals/module2/materials/exercises/decomposition/census.c
@@ -1,1 +1,6 @@
-
+/**
+ * Prompts the user for their name and US state, and then echoes that information back
+ * to the console, making sure to capitalize the first letter of each word.
+ */ 
+ 
+ 
