554dd9f9c1a78680242f17957021ff4dff973d5e
brianrchen <brianreaganchen@gmail.com>
2015-12-11 17:32:19 -0600

    Update houseOfPizza.c

--- calendar/cs50/unit1-fundamentals/module3/materials/exercises/spread-out/houseOfPizza.c
+++ calendar/cs50/unit1-fundamentals/module3/materials/exercises/spread-out/houseOfPizza.c
@@ -16,12 +16,12 @@ int main(void)
     int slicesPerPizza = 8;
     float pricePerPizza = 10.00;
 
-    // Display a message for the user, ask how many orange slices they want
-    printf("Welcome to Delmar House of Pizza!\n How many slices do you want?");
+    // Display a message for the user, ask how many pizza slices they want
+    printf("Welcome to Delmar House of Pizza!\n How many slices do you want? ");
     int numberOfSlices = GetInt();
 
     // Get the number of pizzas to order
     int numberOfPizzas = numberOfContainers(numberOfSlices, slicesPerPizza);
     float totalCost = numberOfPizzas*pricePerPizza;
-    printf("You need %i pizzas for %i slices.\nThat'll be %.2f!\n");
+    printf("\n\nYou need %i pizzas for %i slices.\nThat'll be %.2f!\n", numberOfPizzas, numberOfSlices, totalCost);
 }
