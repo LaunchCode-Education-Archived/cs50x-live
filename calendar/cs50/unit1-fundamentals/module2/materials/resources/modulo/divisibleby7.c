cfbef710a1b0fb353523922a02c12328ebd92b52
stlou <zach@launchcode.org>
2015-12-11 18:11:25 -0600

    Update divisibleby7.c

--- calendar/cs50/unit1-fundamentals/module2/materials/resources/modulo/divisibleby7.c
+++ calendar/cs50/unit1-fundamentals/module2/materials/resources/modulo/divisibleby7.c
@@ -1,13 +1,13 @@
 /**
-* Says if a number is divisible by 7
-*/
+ * Says if a number is divisible by 7
+ */
 
 #include <stdio.h>
 #include <cs50.h>
 
 int main(void)
 {
-  printf("Give me a number:"); // asks user for a number
+  printf("Give me a number: "); // asks user for a number
   int num = GetInt();
   
   if (num % 7 == 0) // uses modulo operator to see if number is divisible by 7
