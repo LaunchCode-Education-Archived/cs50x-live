5ed14cf2d6fe7b585001a9dbd488401d15c16442
Jesse Levine <jesseilev@gmail.com>
2016-01-08 12:50:04 -0600

    delete Decomposition

--- calendar/cs50/unit1-fundamentals/module2/materials/exercises/decomposition/census.c
+++
@@ -1,6 +0,0 @@
-/**
- * Prompts the user for their name and US state, and then echoes that information back
- * to the console, making sure to capitalize the first letter of each word.
- */ 
- 
- // TODO
