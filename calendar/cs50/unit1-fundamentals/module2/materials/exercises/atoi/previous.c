e9ba8582f3b6d1c7ed8bef9756d4c6f98237dea1
Jesse Levine <jesseilev@gmail.com>
2016-01-08 13:45:11 -0600

    Create previous.c

---
+++ calendar/cs50/unit1-fundamentals/module2/materials/exercises/atoi/previous.c
@@ -0,0 +1,16 @@
+#include<cs50.h>
+#include<stdio.h>
+
+int main ()
+{
+
+  if (argc != 1) 
+  {
+    printf("Please provide one integer.\n")
+    printf("usage: previous n\n");
+  }
+  
+	string input = argv[0];
+	
+	printf ("The previous number is %i\n", input - 1);
+}
