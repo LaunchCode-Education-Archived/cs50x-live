6d7c637f53f5c85a899f5f47221408a8d1dac647
Jesse Levine <jesseilev@gmail.com>
2016-01-08 13:49:32 -0600

    Update previous.c

--- calendar/cs50/unit1-fundamentals/module2/materials/exercises/atoi/previous.c
+++ calendar/cs50/unit1-fundamentals/module2/materials/exercises/atoi/previous.c
@@ -4,13 +4,16 @@
 int main ()
 {
 
-  if (argc != 1) 
+  // validation: make sure they provided a CL argument
+  if (argc < 1) 
   {
     printf("Please provide one integer.\n");
     printf("usage: previous n\n");
+    return 1;
   }
   
-	string input = argv[0];
-	
-	printf ("The previous number is %i\n", input - 1);
+  string input = argv[0];
+  printf ("The previous number is %i\n", input - 1);
+  
+  return 0;
 }
