e5915cedfcc7f7bdee0fe2ba9a236a2c0081a65e
Jesse Levine <jesseilev@gmail.com>
2016-01-08 16:10:34 -0600

    Update census-1.c

--- calendar/cs50/unit1-fundamentals/module2/materials/exercises/census/census-1.c
+++ calendar/cs50/unit1-fundamentals/module2/materials/exercises/census/census-1.c
@@ -2,10 +2,14 @@
 #include <stdio.h>
 #include <string.h>
 
+string promptAndValidate(string fieldname);
+
 int main(void)
 {
     printf("Hello, thank you for signing up for: The United States\n");
     printf("I'll need to get some info from you please...\n\n");
+    
+    // TODO replace lines 14 - 44 with something much nicer
     
     // get first name and make sure it's valid
     string firstname = "";
@@ -42,4 +46,16 @@ int main(void)
     printf("\nGreat thanks, lemme just enter that into my system here...\n");
     printf("%s %s from %s, at %s\n", firstname, lastname, state, email);
     printf("Excellent! Please pay your taxes in April, or you'll be hearing from us (check your spam folder).\n");
+}
+
+
+string promptAndValidate(string fieldname)
+{
+    string answer = "";
+    while( strlen(answer) < 2 ) 
+    {
+        printf("%s (must be at least 2 characters long):\n", fieldname);
+        answer = GetString();
+    }  
+    return answer;
 }
