280e9a700b88b0c793aae90dcdd7a8e1c6a6dc45
Jesse Levine <jesseilev@gmail.com>
2016-02-04 15:23:00 -0600

    Create codify.c

---
+++ calendar/cs50/unit2-advanced-c/module4/materials/studios/ascii-art/codify/codify.c
@@ -0,0 +1,106 @@
+// For Staff
+// Use this program to create CSV files from BPM images. 
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <cs50.h>
+
+#include "bmp.h"
+
+bool isDark(RGBTRIPLE pixel, int cutoff);
+
+int main(int argc, char* argv[])
+{
+    // ensure proper usage
+    if (argc != 3)
+    {
+        printf("Usage: copy infile outfile\n");
+        return 1;
+    }
+
+    // remember filenames
+    char* infile = argv[1];
+    char* outfile = argv[2];
+
+    // open input file 
+    FILE* inptr = fopen(infile, "r");
+    if (inptr == NULL)
+    {
+        printf("Could not open %s.\n", infile);
+        return 2;
+    }
+
+    // open output file
+    FILE* outptr = fopen(outfile, "w");
+    if (outptr == NULL)
+    {
+        fclose(inptr);
+        fprintf(stderr, "Could not create %s.\n", outfile);
+        return 3;
+    }
+
+    // read infile's BITMAPFILEHEADER
+    BITMAPFILEHEADER bf;
+    fread(&bf, sizeof(BITMAPFILEHEADER), 1, inptr);
+
+    // read infile's BITMAPINFOHEADER
+    BITMAPINFOHEADER bi;
+    fread(&bi, sizeof(BITMAPINFOHEADER), 1, inptr);
+
+    // ensure infile is (likely) a 24-bit uncompressed BMP 4.0
+    if (bf.bfType != 0x4d42 || bf.bfOffBits != 54 || bi.biSize != 40 || 
+        bi.biBitCount != 24 || bi.biCompression != 0)
+    {
+        fclose(outptr);
+        fclose(inptr);
+        fprintf(stderr, "Unsupported file format.\n");
+        return 4;
+    }
+
+    // determine padding for scanlines
+    int padding =  (4 - (bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
+
+    // iterate over infile's scanlines
+    for (int i = 0, biHeight = abs(bi.biHeight); i < biHeight; i++)
+    {
+        // iterate over pixels in scanline
+        for (int j = 0; j < bi.biWidth; j++)
+        {
+            // temporary storage
+            RGBTRIPLE triple;
+
+            // read RGB triple from infile
+            fread(&triple, sizeof(RGBTRIPLE), 1, inptr);
+            
+            // add a marker if the pixel is dark enough
+            if (isDark(triple, 128)) 
+            {
+               fprintf(outptr, "%i,", j);
+            }
+        }
+
+        // skip over padding in input file, if any
+        fseek(inptr, padding, SEEK_CUR);
+
+        // add new line to output file
+        fputc('.', outptr);
+        fputc('\n', outptr);
+    }
+
+    // close infile
+    fclose(inptr);
+
+    // close outfile
+    fclose(outptr);
+
+    // that's all folks
+    return 0;
+}
+
+
+bool isDark(RGBTRIPLE pixel, int cutoff)
+{
+    float avg = (pixel.rgbtRed + pixel.rgbtGreen + pixel.rgbtBlue) / 3.0;
+    return avg < cutoff;
+}
