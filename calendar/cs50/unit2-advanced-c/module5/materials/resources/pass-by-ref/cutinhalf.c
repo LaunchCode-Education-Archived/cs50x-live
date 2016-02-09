84a2e240a08b30d1bb4d1c1e3af0de13c8533453
Jesse Levine <jesseilev@gmail.com>
2016-02-09 15:40:09 -0600

    Create cutinhalf.c

---
+++ calendar/cs50/unit2-advanced-c/module5/materials/resources/pass-by-ref/cutinhalf.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <cs50.h>
+
+
+void cutInHalf(float* n);
+
+int main (int argc, char* argv[])
+{
+    if (argc != 2)
+    {
+        printf("usage: ./cutinhalf number\n");
+        return 1;
+    }
+    
+    float n = atof(argv[1]);
+    
+    cutInHalf(&n);
+    
+    printf("I halved the number you gave me\n");
+    printf("It is now %f\n", n);
+}
+
+void cutInHalf(float* n)
+{
+    *n = *n / 2;
+}
