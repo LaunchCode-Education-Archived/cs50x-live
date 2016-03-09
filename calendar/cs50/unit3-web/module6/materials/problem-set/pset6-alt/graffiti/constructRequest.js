24f57d216d33da0b32eacd62549b100596184377
Jesse Levine <jesseilev@gmail.com>
2016-03-09 14:33:13 -0600

    Update constructRequest.js

--- calendar/cs50/unit3-web/module6/materials/problem-set/pset6-alt/graffiti/constructRequest.js
+++ calendar/cs50/unit3-web/module6/materials/problem-set/pset6-alt/graffiti/constructRequest.js
@@ -3,13 +3,22 @@ var slackForm = document.getElementById("slackForm");
 slackForm.onsubmit = populatePayloadField;
 
 function populatePayloadField() {
+
+	// convert form fields into a JSON string
 	var formData = getFormData();
 	delete formData.payload;
 	jsonString = JSON.stringify(formData);
-	document.getElementById("payload").value = jsonString;
+
+	// create a new hidden input and add it to the form
+	var payloadInput = document.createElement("input");
+	payloadInput.setAttribute("type", "hidden");
+	payloadInput.setAttribute("name", "payload");
+	payloadInput.setAttribute("value", jsonString);
+	slackForm.appendChild(payloadInput);
 };
 
 function getFormData() {
+	// return a javascript object containing all the form's fields
 	var object = {};
 	var fields = $("#slackForm").serializeArray();
 	for (var i = 0; i < fields.length; i++) {
