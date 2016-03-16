dc6fee76d6c015c5e74bf4e6bf2dc0e32691c780
Jesse Levine <jesseilev@gmail.com>
2016-03-16 15:48:35 -0500

    Update hello-form.php

--- calendar/cs50/unit3-web/module7/materials/exercises/hello2/hello-form.php
+++ calendar/cs50/unit3-web/module7/materials/exercises/hello2/hello-form.php
@@ -4,11 +4,18 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Hello 0</title>
+        <title>Hello 2</title>
     </head>
     <body>
         <form action="hello-response.php" method="GET">
-            <label>Say hello to</label>
+            <label>Say</label>
+            <select name="greeting">
+                <option value="">*[Select a greeting]</option>
+                <option value="Hello">Hello</option>
+                <option value="Hi">Hi</option>
+                <option value="Top o' the mornin">Top o' the mornin</option>
+            </select>
+            <label>to</label>
             <input type="text" name="username"/>
             <input type="submit" value="Submit"/>
         </form>
