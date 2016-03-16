4277b949194fb5d045919234cc4f06dc92d11eca
Jesse Levine <jesseilev@gmail.com>
2016-03-16 15:28:10 -0500

    Update hello-response.php

--- calendar/cs50/unit3-web/module7/materials/exercises/hello1/hello-response.php
+++ calendar/cs50/unit3-web/module7/materials/exercises/hello1/hello-response.php
@@ -3,9 +3,27 @@
 ```php
 <?php 
 
-$greeting = $_GET["greeting"];
-$username = $_GET["username"];
-echo("$greeting, $username!");
+$username = $_GET["username"]; 
+// TODO we also need to grab the greeting field that the user typed into the form
+
+
+if (empty($username)): // TODO we also want to check if the greeting is an empty value
+
+    // redirect the user back to the index page
+    header("Location: ./hello-form.php"); // notice we can use a "relative" link here
+    
+    // stop running this script
+    exit;
+endif
 
 ?>
+
+<!DOCTYPE html>
+<head>
+    <title>Well hello there</title>
+</head>
+<body>
+    Hello, <?php print($username); ?>!
+    <!-- TODO replace the hard-coded "Hello" above with the user-supplied greeting -->
+</body>
 ```
